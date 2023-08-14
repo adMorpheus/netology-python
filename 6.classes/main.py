908240a86d7ee225bdea0555784b5f7e18fd4a7b
Dmitriy Dubrovin <dmitriy.dubrovin.87@gmail.com>
2023-08-14 19:18:55 +0500

    oop-2

--- 6.classes/main.py
+++ 6.classes/main.py
@@ -1,86 +1,93 @@
 class Animals:
     def __init__(self):
-        self.name = ''
-        self.kind_of_animal = str(type(self)).split('.')[1][:-2]
-        self.weight = 0
-        self.type_of_join = ''
-        self.type_of_voice = ''
-        self.type_of_corn = ''
-
+        self._name = ''
+        self._kind_of_animal = str(type(self)).split('.')[1][:-2]
+        self._weight = 0
+        self._type_of_join = ''
+        self._type_of_voice = ''
+        self._type_of_corn = ''
 
     def feed(self):
-        return f'{self.name} is fed'
+        return f'{self._name} is fed'
 
     def get_join(self):
-        return f'Some {self.type_of_join}'
+        return f'Some {self._type_of_join}'
 
     def voice(self):
-        return f'Say {self.type_of_voice}'
-    def to_string(self):
-        return f'''{self.kind_of_animal.title()} 
-        Name is: {self.name} 
-        Join: {self.type_of_join}
-        Voice: {self.type_of_voice} 
-        Corn: {self.type_of_corn}
+        return f'Say {self._type_of_voice}'
+
+    def __str__(self):
+        return f'''{self._kind_of_animal.title()} 
+        Name is: {self._name} 
+        Join: {self._type_of_join}
+        Voice: {self._type_of_voice} 
+        Corn: {self._type_of_corn}
         '''
-# animal = Animals()
-# print(animal.to_string())
 
-class Goose(Animals):
+
+class Poultry(Animals):
+    def __init__(self):
+        super().__init__()
+        self._type_of_join = 'eggs'
+        self._type_of_corn = 'corn'
+
+
+class Livestosk(Animals):
+    def __init__(self):
+        super().__init__()
+        self._type_of_join = 'milk'
+        self._type_of_corn = 'grass'
+
+
+
+
+class Goose(Poultry):
     def __init__(self, name, weight):
-        self.name = name
-        self.kind_of_animal = str(type(self)).split('.')[1][:-2]
-        self.weight = weight
-        self.type_of_join = 'eggs'
-        self.type_of_voice = 'krya'
-        self.type_of_corn = 'grass'
+        super().__init__()
+        self._name = name
+        self._weight = weight
+        self._type_of_voice = 'krya'
 
-class Cow(Animals):
+
+class Cow(Livestosk):
     def __init__(self, name, weight):
-        self.name = name
-        self.kind_of_animal = str(type(self)).split('.')[1][:-2]
-        self.weight = weight
-        self.type_of_join = 'milk'
-        self.type_of_voice = 'Moooo'
-        self.type_of_corn = 'grass'
+        super().__init__()
+        self._name = name
+        self._weight = weight
+        self._type_of_voice = 'Moooo'
 
-class Sheep(Animals):
+
+class Sheep(Livestosk):
     def __init__(self, name, weight):
-        self.name = name
-        self.kind_of_animal = str(type(self)).split('.')[1][:-2]
-        self.weight = weight
-        self.type_of_join = 'wool'
-        self.type_of_voice = 'bueee'
-        self.type_of_corn = 'grass'
+        super().__init__()
+        self._name = name
+        self._weight = weight
+        self._type_of_join = 'wool'
+        self._type_of_voice = 'bueee'
 
-class Hen(Animals):
+
+class Hen(Poultry):
     def __init__(self, name, weight):
-        self.name = name
-        self.kind_of_animal = str(type(self)).split('.')[1][:-2]
-        self.weight = weight
-        self.type_of_join = 'eggs'
-        self.type_of_voice = 'kudah'
-        self.type_of_corn = 'corn'
+        super().__init__()
+        self._name = name
+        self._weight = weight
+        self._type_of_voice = 'kudah'
 
-class Goat(Animals):
+
+class Goat(Livestosk):
     def __init__(self, name, weight):
-        self.name = name
-        self.kind_of_animal = str(type(self)).split('.')[1][:-2]
-        self.weight = weight
-        self.type_of_join = 'milk'
-        self.type_of_voice = 'bueeeee'
-        self.type_of_corn = 'grass'
+        super().__init__()
+        self._name = name
+        self._weight = weight
+        self._type_of_voice = 'bueeeee'
 
-class Duck(Animals):
+
+class Duck(Poultry):
     def __init__(self, name, weight):
-        self.name = name
-        self.kind_of_animal = str(type(self)).split('.')[1][:-2]
-        self.weight = weight
-        self.type_of_join = 'eggs'
-        self.type_of_voice = 'krya'
-        self.type_of_corn = 'grass'
-
-
+        super().__init__()
+        self._name = name
+        self._weight = weight
+        self._type_of_voice = 'krya'
 
 
 class Main():
@@ -95,27 +102,33 @@ class Main():
             Goat('Hooves', 40),
             Duck('Kryakva', 3)
             }
+
     def farm_sound(self):
         for say in self.farm:
             print(say.voice(), end=' ')
 
+    def feed_farm(self):
+        for animal in self.farm:
+            print(f'{animal.feed()} {animal._type_of_corn}')
+
+
     def max_weight(self):
-        max_weight_animal = 'Uncle Joue' # jff
+        max_weight_animal = 'Uncle Joue'  # jff
         max_weight = -1
         for animal in self.farm:
-            if animal.weight > max_weight:
+            if animal._weight > max_weight:
                 max_weight_animal = animal
-                max_weight = animal.weight
+                max_weight = animal._weight
         return max_weight_animal
+
     def common_weight(self):
         common_weight = 0
         for animal in self.farm:
-            common_weight += animal.weight
+            common_weight += animal._weight
         return common_weight
 
 
-
-
 Main().farm_sound()
-print(Main().max_weight().to_string())
+print(Main().max_weight())
+Main().feed_farm()
 print(Main().common_weight())
