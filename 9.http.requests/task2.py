46b8a7c98182dbd5a9adb80a58e82e3423ab6efe
dubrovin_da <dubrovin_da@ust.ru>
2023-08-17 16:11:31 +0500

    9.http.requests 2 task solution

--- 9.http.requests/task2.py
+++ 9.http.requests/task2.py
@@ -1,4 +1,8 @@
+from pprint import pprint
+
 import yandex_disk_creds
+import requests
+
 
 # yandex_disk_creds.OAUTH_TOKEN
 class YaUploader:
@@ -7,10 +11,19 @@ class YaUploader:
 
     def upload(self, file_path: str):
         """Метод загруджает файл file_path на яндекс диск"""
-        # Тут ваша логика
-        return 'Вернуть ответ об успешной загрузке'
+        headers = {'Authorization': self.token}
+        params = {'path': '/sticker.jpg'}
+        url = 'https://cloud-api.yandex.net:443/v1/disk/resources/upload'
+        resp = requests.get(url, headers=headers, params=params)
+        resp.raise_for_status()
+        upload_link = resp.json()['href']
+        with open(file_path, 'rb') as src_file:
+            resp = requests.put(upload_link, src_file.read())
+            resp.raise_for_status()
+
+            return 'File loaded successfuly'
 
 
 if __name__ == '__main__':
     uploader = YaUploader(yandex_disk_creds.OAUTH_TOKEN)
-    result = uploader.upload('c:\my_folder\file.txt')
+    result = uploader.upload('c:\sticker.jpg')
